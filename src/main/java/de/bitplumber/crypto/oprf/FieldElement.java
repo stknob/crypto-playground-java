15deabc4c94937c66ad560ab425f341d3c6e8933
Stefan Knoblich <stkn@bitplumber.de>
2025-06-06 23:58:07 +0200

    oprf: Remove unused interfaces
    Signed-off-by: Stefan Knoblich <stkn@bitplumber.de>

--- src/main/java/de/bitplumber/crypto/oprf/FieldElement.java
+++
@@ -1,9 +0,0 @@
-package de.bitplumber.crypto.oprf;
-
-public interface FieldElement {
-	public FieldElement add(FieldElement other);
-	public FieldElement subtract(FieldElement other);
-	public FieldElement multiply(FieldElement other);
-	public FieldElement invert();
-	public byte[] toByteArray();
-}
--- src/main/java/de/bitplumber/crypto/oprf/GroupElement.java
+++
@@ -1,8 +0,0 @@
-package de.bitplumber.crypto.oprf;
-
-public interface GroupElement {
-	public GroupElement add(GroupElement other);
-	public GroupElement subtract(GroupElement other);
-	public GroupElement multiply(FieldElement scalar);
-	public byte[] toByteArray();
-}
