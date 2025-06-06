4508fcef36426f05302a1323db51d8751ac48533
Stefan Knoblich <stkn@bitplumber.de>
2025-06-07 00:00:05 +0200

    oprf: Remove unused imports
    Signed-off-by: Stefan Knoblich <stkn@bitplumber.de>

--- src/main/java/de/bitplumber/crypto/oprf/Voprf.java
+++ src/main/java/de/bitplumber/crypto/oprf/Voprf.java
@@ -1,8 +1,5 @@
 package de.bitplumber.crypto.oprf;
 
-import com.weavechain.curve25519.RistrettoElement;
-import com.weavechain.curve25519.Scalar;
-
 public interface Voprf<S, E, BR, BER, P> {
 	public KeyPair deriveKeyPair(byte[] seed, byte[] info) throws Exception;
 	public KeyPair randomKeyPair();
